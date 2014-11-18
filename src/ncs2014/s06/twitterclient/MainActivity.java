881fa7f89faec0a6f31350725e4a63a02dc8e36f
1216006 <1216006@A30222.student.ncs.ac.jp>
2014-11-18 14:12:14 +0900

    つるっぴー201411181412

--- src/ncs2014/s06/twitterclient/MainActivity.java
+++ src/ncs2014/s06/twitterclient/MainActivity.java
@@ -1,14 +1,22 @@
 package ncs2014.s06.twitterclient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.twitter_home);
+
+        if (!TwitterUtils.hasAccessToken(this)) {
+            Intent intent = new Intent(this, TwitterOAuthActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 
 }
