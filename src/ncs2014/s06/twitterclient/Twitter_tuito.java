19224fcfacf5f114f68ff9c685dd58b8d0e48066
1216028 <1216028@A30224.student.ncs.ac.jp>
2014-11-20 14:33:27 +0900

    ツイート実装

--- src/ncs2014/s06/twitterclient/Twitter_tuito.java
+++ src/ncs2014/s06/twitterclient/Twitter_tuito.java
@@ -1,14 +1,62 @@
 package ncs2014.s06.twitterclient;
-
-import android.app.Activity;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Twitter_tuito extends Activity {
+public class Twitter_tuito extends FragmentActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.twittertuito);
-	}
+    private EditText mInputText;
+    private Twitter mTwitter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_tweet);
+
+        mTwitter = TwitterUtils.getTwitterInstance(this);
+
+        mInputText = (EditText) findViewById(R.id.input_text);
+
+        findViewById(R.id.action_tweet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tweet();
+            }
+        });
+    }
+
+    private void tweet() {
+        AsyncTask<String, Void, Boolean> task = new AsyncTask<String, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... params) {
+                try {
+                    mTwitter.updateStatus(params[0]);
+                    return true;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                if (result) {
+                    showToast("ツイートが完了しました！");
+                    finish();
+                } else {
+                    showToast("ツイートに失敗しました。。。");
+                }
+            }
+        };
+        task.execute(mInputText.getText().toString());
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
 }
