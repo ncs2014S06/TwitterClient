16a34cf130d98e2fcfa85fc6bfea4c6546d732da
syuritanhaahaa <hawawaawawa@gmail.com>
2014-11-21 10:33:09 +0900

    TL修正　201411211033

--- src/ncs2014/s06/twitterclient/Twitter_home.java
+++ src/ncs2014/s06/twitterclient/Twitter_home.java
@@ -1,9 +1,15 @@
 package ncs2014.s06.twitterclient;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.ResponseList;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -11,6 +17,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Twitter_home extends Activity implements OnClickListener{
 
@@ -23,6 +31,8 @@ public class Twitter_home extends Activity implements OnClickListener{
 	private Button bt2;
 	private TweetAdapter tAdapter;
 	private Twitter mTwitter;
+	private ListView list;
+
 	//intent
 	Intent intent = new Intent();
 
@@ -35,6 +45,7 @@ public class Twitter_home extends Activity implements OnClickListener{
 		//findview
 		bt1 = (Button) findViewById(R.id.bt1);
 		bt2 = (Button) findViewById(R.id.bt2);
+		list = (ListView) findViewById(R.id.tllist);
 
 		//リスナー
 		bt1.setOnClickListener(this);
@@ -46,11 +57,50 @@ public class Twitter_home extends Activity implements OnClickListener{
 			finish();
 		}else{
 			tAdapter = new TweetAdapter(this);
-
-
 			mTwitter = TwitterUtils.getTwitterInstance(this);
+			reloadTimeLine();
+			list.setAdapter(tAdapter);
 		}
 	}//onCreate
+
+
+	private void reloadTimeLine() {
+		AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>() {
+			@Override
+			protected List<String> doInBackground(Void... params) {
+				try {
+					ResponseList<twitter4j.Status> timeline = mTwitter.getHomeTimeline();
+					ArrayList<String> list = new ArrayList<String>();
+					for (twitter4j.Status status : timeline) {
+						list.add(status.getText());
+					}
+					return list;
+				} catch (TwitterException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(List<String> result) {
+				if (result != null) {
+					tAdapter.clear();
+					for (String status : result) {
+						tAdapter.add(status);
+					}
+					//la.getListView().setSelection(0);
+				} else {
+					showToast("タイムラインの取得に失敗しました。。。");
+				}
+			}
+		};
+		task.execute();
+	}
+
+	private void showToast(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -60,10 +110,10 @@ public class Twitter_home extends Activity implements OnClickListener{
 			return true;
 
 			case MENU_B:
-				startActivity(new Intent(
-					Twitter_home.this,
-					Twitter_user.class)
-				);
+					startActivity(new Intent(
+						Twitter_home.this,
+						Twitter_user.class)
+					);
 			return true;
 
 			case MENU_C:
@@ -81,8 +131,8 @@ public class Twitter_home extends Activity implements OnClickListener{
 	 */
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		return true;
-    }
+		 return true;
+	}
 
 	@Override
 	public void onClick(View v) {
@@ -107,9 +157,9 @@ public class Twitter_home extends Activity implements OnClickListener{
 	//表示定義クラス
 	private class TweetAdapter extends ArrayAdapter<String> {
 
-		public TweetAdapter(Context context) {
-			super(context, android.R.layout.simple_list_item_1);
-		}
+		 public TweetAdapter(Context context) {
+			  super(context, android.R.layout.simple_list_item_1);
+		 }
 	}//TweetAdapter
 
 
