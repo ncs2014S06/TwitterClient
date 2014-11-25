5ee38690a7022a39bf68b244b5aad546fc94b0a5
syuritanhaahaa <hawawaawawa@gmail.com>
2014-11-25 14:56:47 +0900

    201411251456

--- src/ncs2014/s06/twitterclient/Twitter_home.java
+++ src/ncs2014/s06/twitterclient/Twitter_home.java
@@ -1,9 +1,8 @@
 package ncs2014.s06.twitterclient;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import android.app.Activity;
@@ -11,15 +10,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.loopj.android.image.SmartImageView;
 
 public class Twitter_home extends Activity implements OnClickListener{
 
@@ -66,16 +70,11 @@ public class Twitter_home extends Activity implements OnClickListener{
 
 
 	private void reloadTimeLine() {
-		AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>() {
+		AsyncTask<Void, Void, List<twitter4j.Status>> task = new AsyncTask<Void, Void, List<twitter4j.Status>>() {
 			@Override
-			protected List<String> doInBackground(Void... params) {
+			protected List<twitter4j.Status> doInBackground(Void... params) {
 				try {
-					ResponseList<twitter4j.Status> timeline = mTwitter.getHomeTimeline();
-					ArrayList<String> list = new ArrayList<String>();
-					for (twitter4j.Status status : timeline) {
-						list.add(status.getText());
-					}
-					return list;
+					return mTwitter.getHomeTimeline();
 				} catch (TwitterException e) {
 					e.printStackTrace();
 				}
@@ -83,10 +82,10 @@ public class Twitter_home extends Activity implements OnClickListener{
 			}
 
 			@Override
-			protected void onPostExecute(List<String> result) {
+			protected void onPostExecute(List<twitter4j.Status> result) {
 				if (result != null) {
 					tAdapter.clear();
-					for (String status : result) {
+					for (twitter4j.Status status : result) {
 						tAdapter.add(status);
 					}
 					//la.getListView().setSelection(0);
@@ -180,11 +179,35 @@ public class Twitter_home extends Activity implements OnClickListener{
 
 	}
 	//表示定義クラス
-	private class TweetAdapter extends ArrayAdapter<String> {
+	private class TweetAdapter extends ArrayAdapter<twitter4j.Status> {
 
-		 public TweetAdapter(Context context) {
-			  super(context, android.R.layout.simple_list_item_1);
-		 }
+		private LayoutInflater mInflater;
+
+		public TweetAdapter(Context context) {
+			super(context, android.R.layout.simple_list_item_1);
+			mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO 自動生成されたメソッド・スタブ
+
+			if(convertView == null){
+				convertView = mInflater.inflate(R.layout.list_item_tweet, null);
+			}
+
+			Status item = getItem(position);
+			SmartImageView icon = (SmartImageView) convertView.findViewById(R.id.icon);
+			icon.setImageUrl(item.getUser().getProfileImageURL());
+			TextView name = (TextView) convertView.findViewById(R.id.name);
+			name.setText(item.getUser().getName());
+			TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+			screenName.setText("@" + item.getUser().getScreenName());
+			TextView text = (TextView) convertView.findViewById(R.id.text);
+			text.setText(item.getText());
+			return convertView;
+		}
 	}//TweetAdapter
 
 
