c31a697c9299137c27fe94a0786f619e301123cc
1216006 <1216006@A30222.student.ncs.ac.jp>
2014-11-25 13:22:21 +0900

    201411251322

--- src/ncs2014/s06/twitterclient/Twitter_Client_DM.java
+++ src/ncs2014/s06/twitterclient/Twitter_Client_DM.java
@@ -4,75 +4,100 @@ import java.util.List;
 
 import twitter4j.DirectMessage;
 import twitter4j.Paging;
+import twitter4j.ResponseList;
 import twitter4j.Twitter;
-import twitter4j.TwitterAdapter;
 import twitter4j.TwitterException;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class Twitter_Client_DM extends FragmentActivity {
 
 	 private Twitter mTwitter;
-    private ListView lv ;
-    private ArrayAdapter<String> adapter;
-    private TwitterAdapter mAdapter;
-    List<String> list = new ArrayList<String>();
+	private TweetAdapter tAdapter;
+	private ListView list;
 
 
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.twitter_dmlist);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.twitter_dmlist);
 
-        mTwitter = TwitterUtils.getTwitterInstance(this);
-        lv = (ListView) findViewById(R.id.dmlist);
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		list = (ListView) findViewById(R.id.dmlist);
 
-        if (!TwitterUtils.hasAccessToken(this)) {
-            Intent intent = new Intent(this, TwitterOAuthActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updata();
-            }
-        });
-    }
+		if (!TwitterUtils.hasAccessToken(this)) {
+			Intent intent = new Intent(this, TwitterOAuthActivity.class);
+			startActivity(intent);
+			finish();
+		}else{
+			tAdapter = new TweetAdapter(this);
+			mTwitter = TwitterUtils.getTwitterInstance(this);
+		}
 
 
-	private void updata() {
-    	//Twitter twitter = new TwitterFactory().getInstance();
-        try {
-            Paging paging = new Paging(1);
-            List<DirectMessage> messages;
-            do {
-                //messages = twitter.getDirectMessages(paging);
-                messages = mTwitter.getDirectMessages(paging);
-                for (DirectMessage message : messages) {
-                   /* System.out.println("From: @" + message.getSenderScreenName() + " id:" + message.getId() + " - "
-                            + message.getText()); */
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				reloadDM();
+				list.setAdapter(tAdapter);
+			}
+		});
+	}
 
-               adapter.add(message.getText());
-                }
+	private void reloadDM(){
+		AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>(){
+			@Override
+			protected List<String> doInBackground(Void... params) {
+				// TODO 自動生成されたメソッド・スタブ
+				try {
+					Paging paging = new Paging(1);
+					ResponseList<DirectMessage> messages;
+					ArrayList<String> list = new ArrayList<String>();
+					do {
+						messages = mTwitter.getDirectMessages(paging);
+						for (DirectMessage message : messages) {
+							Log.d("dm", message.getText());
+							list.add(message.getText());
+						}
+						paging.setPage(paging.getPage() + 1);
+					} while (messages.size() > 0 && paging.getPage() < 10);
+					return list;
+				} catch (TwitterException te) {
+					te.printStackTrace();
+				}
+				return null;
+			}
+			@Override
+			protected void onPostExecute(List<String> result) {
+				// TODO 自動生成されたメソッド・スタブ
+				if(result != null){
+					tAdapter.clear();
+					for(String status:result){
+						tAdapter.add(status);
+					}
+				}else{
+					showToast("DMの取得に失敗しました");
+				}
+			}
+		};
+		task.execute();
+	}
 
-                lv.setAdapter(adapter);
-
-                paging.setPage(paging.getPage() + 1);
-            } while (messages.size() > 0 && paging.getPage() < 10);
-            System.exit(0);
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.exit(-1);
-            }
+	private void showToast(String text){
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
 
 
-    }
-
+	private class TweetAdapter extends ArrayAdapter<String>{
+		public TweetAdapter(Context context){
+			super(context,android.R.layout.simple_list_item_1);
+		}
+	}
 }
