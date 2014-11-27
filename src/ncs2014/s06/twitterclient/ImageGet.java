4aae03578dd02538e5e4ac1003cb43bbe17df5f2
syuritanhaahaa <hawawaawawa@gmail.com>
2014-11-27 13:39:38 +0900

    2014/11/27 1339

--- src/ncs2014/s06/twitterclient/ImageGet.java
+++ src/ncs2014/s06/twitterclient/ImageGet.java
@@ -12,6 +12,7 @@ import com.loopj.android.image.SmartImageView;
 public class ImageGet extends Activity{
 	private Twitter mTwitter;
 	private User user;
+	private String name;
 	private SmartImageView view;
 
 
@@ -19,16 +20,61 @@ public class ImageGet extends Activity{
 		this.mTwitter = mTwitter;
 	}
 
-
+	/**
+	 * ログインユーザーのアイコンをview1にセットする
+	 * @param view1
+	 */
 	public void setImage(SmartImageView view1){
 		this.view = view1;
+		user = null;
 		AsyncTask<Void, Void, User> task = new AsyncTask<Void,Void,User>(){
 
 			@Override
 			protected User doInBackground(Void... params) {
 				// TODO 自動生成されたメソッド・スタブ
 				try {
+					Log.d("test",mTwitter.getScreenName());
 					User test = mTwitter.showUser(mTwitter.getScreenName());
+					return test;
+				} catch (TwitterException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+					Log.d("test","error");
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(User result) {
+				// TODO 自動生成されたメソッド・スタブ
+				if(result != null){
+					user = result;
+					view.setImageUrl(user.getProfileImageURL());
+					Log.d("test",user.getScreenName());
+				}else{
+					Log.d("test","null1");
+				}
+			}
+		};
+		task.execute();
+	}
+
+	/**
+	 * name1のアイコンをview1にセットする
+	 * @param view1
+	 * @param name1
+	 */
+	public void setImage(SmartImageView view1,String name1){
+		this.view = view1;
+		this.name = name1;
+		user = null;
+		AsyncTask<Void, Void, User> task = new AsyncTask<Void,Void,User>(){
+
+			@Override
+			protected User doInBackground(Void... params) {
+				// TODO 自動生成されたメソッド・スタブ
+				try {
+					User test = mTwitter.showUser(name);
 					return test;
 				} catch (TwitterException e) {
 					// TODO 自動生成された catch ブロック
--- src/ncs2014/s06/twitterclient/Twitter_user.java
+++ src/ncs2014/s06/twitterclient/Twitter_user.java
@@ -1,10 +1,7 @@
 package ncs2014.s06.twitterclient;
 
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.User;
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -16,18 +13,18 @@ public class Twitter_user extends Activity {
 
 	private SmartImageView view;
 	private Twitter mTwitter;
-	private User user;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mTwitter = TwitterUtils.getTwitterInstance(this);
+		ImageGet ig = new ImageGet(mTwitter);
 		Log.d("test","test");
 		setContentView(R.layout.twitteruser);
 		ListView listView = (ListView) findViewById(R.id.aaa);
 		view = (SmartImageView) findViewById(R.id.imageView1);
 
-		new ImageGet(mTwitter).setImage(view);
+		ig.setImage(view);
 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
 
@@ -36,38 +33,6 @@ public class Twitter_user extends Activity {
 		 adapter.add("c");
 
 		 listView.setAdapter(adapter);
-	}
-
-	private void setImage(){
-		AsyncTask<Void, Void, User> task = new AsyncTask<Void,Void,User>(){
-
-			@Override
-			protected User doInBackground(Void... params) {
-				// TODO 自動生成されたメソッド・スタブ
-				try {
-					User test = mTwitter.showUser(mTwitter.getScreenName());
-					return test;
-				} catch (TwitterException e) {
-					// TODO 自動生成された catch ブロック
-					e.printStackTrace();
-					Log.d("test","error");
-				}
-				return null;
-			}
-
-			@Override
-			protected void onPostExecute(User result) {
-				// TODO 自動生成されたメソッド・スタブ
-				if(result != null){
-					user = result;
-					view.setImageUrl(user.getProfileImageURL());
-					Log.d("test",user.getScreenName());
-				}else{
-					Log.d("test","null1");
-				}
-			}
-		};
-		task.execute();
 	}
 
 
