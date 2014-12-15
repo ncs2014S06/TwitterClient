4f5ad680251093d614ad44ead9a1b5daf5d5071d
1216028 <1216028@A30224.student.ncs.ac.jp>
2014-12-15 10:40:35 +0900

    followGet api無視

---
+++ src/ncs2014/s06/twitterclient/FollowGet.java
@@ -0,0 +1,151 @@
+package ncs2014.s06.twitterclient;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import twitter4j.PagableResponseList;
+import twitter4j.RateLimitStatus;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.User;
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.image.SmartImageView;
+
+public class FollowGet extends Activity {
+
+	private Twitter mTwitter;
+	private ListView list;
+	private ArrayAdapter<User> uAdapter;
+	private ArrayList<User> List;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.twitter_follow);
+		mTwitter = TwitterUtils.getTwitterInstance(this);
+		uAdapter = new userAdapter(this);
+		list = (ListView) findViewById(R.id.tllist);
+		List = new ArrayList<User>();
+		followGet();
+		list.setAdapter(uAdapter);
+
+
+	}
+
+	public void followGet(){
+		AsyncTask<Void, Void, ArrayList<User>> task = new AsyncTask<Void, Void, ArrayList<User>>(){
+
+			@Override
+			protected ArrayList<User> doInBackground(Void... params) {
+				Log.d("test","doinbackground");
+				long i = -1;
+				int j = 0;
+
+
+				try {
+					Log.d("test","try");
+					PagableResponseList<User> user = mTwitter.getFriendsList(mTwitter.getId(), i);
+					do{
+				//	while(j <= 300){
+						for(User u :user){
+							Log.d("Friends",u.getName());
+							List.add(u);
+							Log.d("test","User for");
+						}
+						user = mTwitter.getFriendsList(mTwitter.getId(), user.getNextCursor());
+						Log.d("test","Long i2 " + i);
+					}while(user.hasNext());
+					Log.d("test","for1後");
+				} catch (IllegalStateException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				} catch (TwitterException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+
+				return List;
+			}
+
+			@Override
+			protected void onPostExecute(ArrayList<User> result) {
+				// TODO 自動生成されたメソッド・スタブ
+				super.onPostExecute(result);
+				uAdapter.clear();
+				for(User u:result){
+					uAdapter.add(u);
+				}
+				ApiLimit api = new ApiLimit(mTwitter);
+				api.execute();
+				Map<String, RateLimitStatus> map = null;
+				try {
+					map = api.get();
+				} catch (InterruptedException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+				RateLimitStatus TLlimit = map.get("/friends/list");
+				int i = TLlimit.getSecondsUntilReset();
+				String m = i / 60 + "分";
+				String S = i % 60 + "秒";
+				showToast("残り読み込み回数" + TLlimit.getRemaining() + "回\nAPIリセットまで" + m + S);
+			}
+		};
+		task.execute();
+	}
+	private void showToast(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+
+	//表示定義クラス
+	public class userAdapter extends ArrayAdapter<User> {
+
+		private LayoutInflater mInflater;
+
+		public userAdapter(Context context) {
+			super(context, android.R.layout.simple_list_item_1);
+			mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO 自動生成されたメソッド・スタブ
+
+			if(convertView == null){
+				convertView = mInflater.inflate(R.layout.list_item_user, null);
+			}
+
+
+
+			User item = getItem(position);
+			SmartImageView icon = (SmartImageView) convertView.findViewById(R.id.icon);
+			icon.setImageUrl(item.getProfileImageURL());
+			TextView name = (TextView) convertView.findViewById(R.id.name);
+			name.setText(item.getName());
+			TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+			screenName.setText("@"+ item.getScreenName());
+			TextView text = (TextView) convertView.findViewById(R.id.text);
+		//	text.setText(item.getText());
+			return convertView;
+		}
+
+	}//TweetAdapter
+
+}
