b90bfcc25a20d733b379a559ae06823eee47f880
syuritanhaahaa <hawawaawawa@gmail.com>
2015-02-17 15:17:29 +0900

    02/17 1517

--- src/ncs2014/s06/twitterclient/TwitterSearch.java
+++ src/ncs2014/s06/twitterclient/TwitterSearch.java
@@ -10,12 +10,18 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.User;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
@@ -24,12 +30,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class TwitterSearch extends Activity implements OnClickListener,OnScrollListener,OnItemClickListener{
+public class TwitterSearch extends Activity implements OnClickListener,OnScrollListener,OnItemClickListener, OnTouchListener{
 
 	private Twitter mTwitter;
-	private EditText searchText;
-	private Button searchButton;
-	private ListView searchList;
+	private Context mContext;
 	private TweetAdapter tAdapter;
 	private Paging paging;
 	private User myUser;
@@ -38,7 +42,19 @@ public class TwitterSearch extends Activity implements OnClickListener,OnScrollL
 	private String searchWord;
 	private List<twitter4j.Status> arrayStatus ;
 	private Intent intent = new Intent();
+	private GestureDetector mGestureDetector;
+	private int flingFlag = 0;
+	private static final int FLAG_TRUE = 1;
+	private static final int FLAG_FALSE = 0;
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 	private final static int TWEET_DETAIL = 1000;
+
+
+	private EditText searchText;
+	private Button searchButton;
+	private ListView searchList;
 
 
 	@Override
@@ -48,11 +64,18 @@ public class TwitterSearch extends Activity implements OnClickListener,OnScrollL
 		super.onCreate(savedInstanceState);
 		mTwitter = TwitterUtils.getTwitterInstance(this);
 
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.twitter_search);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_search);
 		searchText = (EditText)findViewById(R.id.searchtext);
 		searchButton = (Button)findViewById(R.id.searchbutton);
 		searchList = (ListView)findViewById(R.id.search_list);
 		searchButton.setOnClickListener(this);
+		searchList.setOnTouchListener(this);
+
+		mContext = getApplicationContext();
+		mGestureDetector = new GestureDetector(mContext,mOnGestureListener);
+
 		tAdapter = new TweetAdapter(this);
 		searchList.setAdapter(tAdapter);
 		searchList.setOnScrollListener(this);
@@ -139,12 +162,15 @@ public class TwitterSearch extends Activity implements OnClickListener,OnScrollL
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
-		Log.d("tweet_detail","view:" + parent + "  position:" + position + "  id:" + id);
-		Status item = (Status) searchList.getItemAtPosition(position);
-		intent.setClass(getApplicationContext(), Twitter_tweet_detail.class);
-		intent.putExtra("TweetStatus", item);
-		intent.putExtra("position", position);
-		startActivityForResult(intent,TWEET_DETAIL);
+		if(flingFlag == FLAG_FALSE){
+			Log.d("tweet_detail","view:" + parent + "  position:" + position + "  id:" + id);
+			Status item = (Status) searchList.getItemAtPosition(position);
+			intent.setClass(getApplicationContext(), Twitter_tweet_detail.class);
+			intent.putExtra("TweetStatus", item);
+			intent.putExtra("position", position);
+			startActivityForResult(intent,TWEET_DETAIL);
+		}
+		flingFlag = FLAG_FALSE;
 	}
 
 	public void exchangeListItem(int position,Status status){
@@ -169,4 +195,49 @@ public class TwitterSearch extends Activity implements OnClickListener,OnScrollL
 			}
 		}
 	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return mGestureDetector.onTouchEvent(event);
+	}
+
+	//横スワイプ処理
+	private final SimpleOnGestureListener mOnGestureListener = new SimpleOnGestureListener() {
+		@Override
+		public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+			flingFlag = FLAG_TRUE;
+			try {
+
+				if (Math.abs(event1.getY() - event2.getY()) > SWIPE_MAX_OFF_PATH) {
+					// 縦の移動距離が大きすぎる場合は無視
+					return false;
+				}
+
+				if (event1.getX() - event2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					// 開始位置から終了位置の移動距離が指定値より大きい
+					// X軸の移動速度が指定値より大きい
+			//		intent.setClass(mContext, Twitter_Streaming.class);
+			//		startActivity(intent);
+					finish();
+					overridePendingTransition(R.anim.right_in, R.anim.left_out);
+
+				} else if (event2.getX() - event1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					// 終了位置から開始位置の移動距離が指定値より大きい
+					// X軸の移動速度が指定値より大きい
+			//		intent.setClass(mContext, TwitterSearch.class);
+			//		startActivity(intent);
+			//		overridePendingTransition(R.anim.left_in, R.anim.right_out);
+				}
+
+			} catch (Exception e) {
+				// nothing
+			}
+			return false;
+		}
+	};
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return mGestureDetector.onTouchEvent(event);
+	}
+
 }
--- src/ncs2014/s06/twitterclient/Twitter_Streaming.java
+++ src/ncs2014/s06/twitterclient/Twitter_Streaming.java
@@ -14,9 +14,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
@@ -24,7 +29,7 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class Twitter_Streaming extends Activity implements OnClickListener, OnItemClickListener {
+public class Twitter_Streaming extends Activity implements OnClickListener, OnItemClickListener, OnTouchListener {
 
 	private Intent intent;
 	private Context mContext;
@@ -33,6 +38,13 @@ public class Twitter_Streaming extends Activity implements OnClickListener, OnIt
 	private TwitterStream mTStream;
 	private TweetAdapter tAdapter;
 	private int streamBtFlag;
+	private GestureDetector mGestureDetector;
+	private int flingFlag = 0;
+	private static final int FLAG_TRUE = 1;
+	private static final int FLAG_FALSE = 0;
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 	private final static int STREAMSTART = 0;
 	private final static int STREAMSTOP = 1;
 	private final static int TWEET_DETAIL = 1000;
@@ -44,12 +56,15 @@ public class Twitter_Streaming extends Activity implements OnClickListener, OnIt
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.twitter_lists);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_streaming);
 		View view = this.getLayoutInflater().inflate(R.layout.twitter_streaming_botans, null);
 		RelativeLayout.LayoutParams lllp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
 		addContentView(view, lllp);
 
 		list = (ListView) findViewById(R.id.twitter_lists_list);
+		list.setOnTouchListener(this);
 		streamingBt = (Button) findViewById(R.id.streaming_streamingbt);
 
 		list.setOnItemClickListener(this);
@@ -57,6 +72,7 @@ public class Twitter_Streaming extends Activity implements OnClickListener, OnIt
 
 		intent = getIntent();
 		mContext = getApplicationContext();
+		mGestureDetector = new GestureDetector(mContext,mOnGestureListener);
 		mHandler = new Handler();
 		AccessToken at = TwitterUtils.loadAccessToken(this);
 		Configuration configuration = new ConfigurationBuilder().setOAuthConsumerKey(getString(R.string.twitter_consumer_key))
@@ -114,11 +130,60 @@ public class Twitter_Streaming extends Activity implements OnClickListener, OnIt
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
-		Log.d("tweet_detail","view:" + parent + "  position:" + position + "  id:" + id);
-		Status item = (Status) list.getItemAtPosition(position);
-		intent.setClass(getApplicationContext(), Twitter_tweet_detail.class);
-		intent.putExtra("TweetStatus", item);
-		intent.putExtra("position", position);
-		startActivityForResult(intent,TWEET_DETAIL);
+		if(flingFlag == FLAG_FALSE){
+			Log.d("tweet_detail","view:" + parent + "  position:" + position + "  id:" + id);
+			Status item = (Status) list.getItemAtPosition(position);
+			intent.setClass(getApplicationContext(), Twitter_tweet_detail.class);
+			intent.putExtra("TweetStatus", item);
+			intent.putExtra("position", position);
+			startActivityForResult(intent,TWEET_DETAIL);
+			}
+		flingFlag = FLAG_FALSE;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return mGestureDetector.onTouchEvent(event);
+	}
+
+	//横スワイプ処理
+	private final SimpleOnGestureListener mOnGestureListener = new SimpleOnGestureListener() {
+		@Override
+		public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+			flingFlag = FLAG_TRUE;
+			try {
+
+				if (Math.abs(event1.getY() - event2.getY()) > SWIPE_MAX_OFF_PATH) {
+					// 縦の移動距離が大きすぎる場合は無視
+					return false;
+				}
+
+				if (event1.getX() - event2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					// 開始位置から終了位置の移動距離が指定値より大きい
+					// X軸の移動速度が指定値より大きい
+			//		intent.setClass(mContext, Twitter_Streaming.class);
+			//		startActivity(intent);
+			//		finish();
+			//		overridePendingTransition(R.anim.right_in, R.anim.left_out);
+
+				} else if (event2.getX() - event1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					// 終了位置から開始位置の移動距離が指定値より大きい
+					// X軸の移動速度が指定値より大きい
+			//		intent.setClass(mContext, TwitterSearch.class);
+			//		startActivity(intent);
+			//		overridePendingTransition(R.anim.left_in, R.anim.right_out);
+					finish();
+					overridePendingTransition(R.anim.left_in, R.anim.right_out);
+				}
+
+			} catch (Exception e) {
+				// nothing
+			}
+			return false;
+		}
+	};
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return mGestureDetector.onTouchEvent(event);
 	}
 }
